7a61f801508a1939c714eaddc84865b6e371cca0
ch4o5 <dongyunli619@gmail.com>
2020-03-16 12:50:44 +0800

    Update nim_doc_lib.sh

--- scripts/nim_doc_lib.sh
+++ scripts/nim_doc_lib.sh
@@ -7,16 +7,19 @@ target="/home/travis/build/nim-lang-cn/website"
 cp -r ${target}/lib_doc/* .
 
 nim_doc_lib_generator(){
-    for file in `ls $1|grep .nim` #注意此处这是两个反引号，表示运行系统命令
+    for file in `ls $1` #注意此处这是两个反引号，表示运行系统命令
         do
         if [ -d $1"/"$file ] #注意此处之间一定要加上空格，否则会报错
             then
                 nim_doc_lib_generator $1"/"$file
             else
-                nim doc $file
-                file_name="${file%.nim}"
-                echo $file_name
-                cp -f ${file_name}.html ${target}/jekyll/_site/docs/
+                if [ ${file%.nim} != ${file} ]
+                    then 
+                        nim doc $file
+                        file_name="${file%.nim}"
+                        echo $file_name
+                        cp -f ${file_name}.html ${target}/jekyll/_site/docs/
+                if
         fi
         done
 } 
