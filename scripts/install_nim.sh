846ae858283cb9b81ec2b0d5bf5a41e441859525
DongyunLee <dongyunli619@gmail.com>
2020-04-17 19:04:04 +0800

    update script and tea

--- scripts/install_nim.sh
+++ scripts/install_nim.sh
@@ -3,12 +3,11 @@
 echo "start install nim"
 
 git clone https://github.com/nim-lang/Nim.git
-cd Nim && sh build_all.sh
+cd Nim 
+./build_all.sh
 
 export NIM=/home/travis/build/nim-lang-cn/website/Nim/bin
 export NIMBLE_DIR=/home/travis/.nimble
 export PATH=$PATH:$NIM:$NIMBLE
-
-source ~/.bashrc 
 
 nim -v
